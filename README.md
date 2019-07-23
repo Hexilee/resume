1ef6f6a33f1bb4e6926a4408e03f1aa5c70acec2
Hexilee <hexileee@gmail.com>
2019-07-23 21:05:40 +0800

    refresh everything

---
+++ README.md
@@ -0,0 +1,20 @@
+# Hexi's resume
+
+This is my resume, based on [this template](https://github.com/ice1000/resume).
+
+# Build
+
+To build this resume, install texlive-full <!-- and [Sarasa Gothic CL](https://github.com/be5invis/Sarasa-Gothic/releases) font --> first.<br/>
+Then:
+
+```bash
+$ make zh
+$ make en
+```
+
+# Download/下载
+
++ [English version (pdf)](./resume.pdf)
+  + [LaTeX Source](./resume.tex)
++ [中文版 (pdf)](./resume-cn.pdf)
+  + [LaTeX 源码](./resume-cn.tex)
---
+++ update.sh
@@ -0,0 +1,51 @@
+echo "=============================="
+echo "==building Chinese version===="
+echo "=============================="
+
+make zh >> make-zh.log
+
+echo "=============================="
+echo "==building English version===="
+echo "=============================="
+
+make en >> make-en.log
+
+echo "=============================="
+echo "====removing useless files===="
+echo "=============================="
+
+rm -rf .git
+rm *~
+rm *.log
+rm *.aux
+rm *.out
+
+echo "================================"
+echo "==reinitialize git repository==="
+echo "================================"
+
+git init
+git remote add origin https://Hexilee@github.com/Hexilee/resume.git
+
+echo "=============================="
+echo "======committing changes======"
+echo "=============================="
+
+git add *
+git add .gitignore
+git stage *
+git commit -a -m "refresh everything" >> commit.log
+git gc >> git-gc.log
+
+echo "=============================="
+echo "=====pushing, please wait====="
+echo "=============================="
+
+git push --force origin HEAD
+git status
+
+echo "=============================="
+echo "========all tasks done========"
+echo "=============================="
+
+rm *.log
